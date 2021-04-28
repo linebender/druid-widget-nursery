31c0c22395cfad19ace2447cb052a9dfb7284111
joshuawade <wadej@southern.edu>
2021-04-28 00:02:09 -0400

    Add core code for managing widget list based on data list

--- examples/repeater.rs
+++ examples/repeater.rs
@@ -1,9 +1,14 @@
-use druid::{im, widget::SizedBox, AppLauncher, Data, Lens, Point, Size, Widget, WindowDesc};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use druid::{
+    im, AppLauncher, BoxConstraints, Color, Data, Env, Event, EventCtx, LayoutCtx, Lens, LifeCycle,
+    LifeCycleCtx, PaintCtx, Point, RenderContext, Size, UpdateCtx, Widget, WidgetPod, WindowDesc,
+};
 
 use druid_widget_nursery::Repeater;
 
 #[derive(Clone, Data)]
-pub struct Window {
+pub struct WindowData {
     pub origin: Point,
     pub size: Size,
     pub id: u64,
@@ -11,7 +16,7 @@ pub struct Window {
 
 #[derive(Clone, Data, Lens)]
 pub struct AppState {
-    windows: im::Vector<Window>,
+    windows: im::Vector<WindowData>,
 }
 
 impl AppState {
@@ -23,11 +28,14 @@ impl AppState {
 }
 
 fn main_widget() -> impl Widget<AppState> {
-    Repeater::new(
-        AppState::windows,
-        Box::new(|window: &Window| window.id),
-        Box::new(|_window: &Window| SizedBox::empty()),
-    )
+    // TODO: Builder pattern
+    RepeaterExample {
+        content: WidgetPod::new(Box::new(Repeater::new(
+            AppState::windows,
+            Box::new(|window: &WindowData| window.id),
+            Box::new(|_window: &WindowData| Window),
+        ))),
+    }
 }
 
 pub fn main() {
@@ -39,4 +47,91 @@ pub fn main() {
         .log_to_console()
         .launch(AppState::new())
         .expect("Failed to launch application");
+}
+
+pub struct Window;
+
+impl Widget<WindowData> for Window {
+    fn event(&mut self, _ctx: &mut EventCtx, _event: &Event, _data: &mut WindowData, _env: &Env) {}
+
+    fn lifecycle(
+        &mut self,
+        _ctx: &mut LifeCycleCtx,
+        _event: &LifeCycle,
+        _data: &WindowData,
+        _env: &Env,
+    ) {
+    }
+
+    fn update(
+        &mut self,
+        _ctx: &mut UpdateCtx,
+        _old_data: &WindowData,
+        _data: &WindowData,
+        _env: &Env,
+    ) {
+    }
+
+    fn layout(
+        &mut self,
+        _ctx: &mut LayoutCtx,
+        bc: &BoxConstraints,
+        _data: &WindowData,
+        _env: &Env,
+    ) -> Size {
+        bc.max()
+    }
+
+    fn paint(&mut self, ctx: &mut PaintCtx, _data: &WindowData, _env: &Env) {
+        let size = ctx.size();
+        let rect = size.to_rect();
+        ctx.fill(rect, &Color::rgba(1., 1., 1., 0.1));
+    }
+}
+
+struct RepeaterExample {
+    pub content: WidgetPod<AppState, Box<dyn Widget<AppState>>>,
+}
+
+impl Widget<AppState> for RepeaterExample {
+    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut AppState, env: &Env) {
+        self.content.event(ctx, event, data, env);
+
+        if let Event::MouseDown(_) = event {
+            let start = SystemTime::now();
+            let since_the_epoch = start
+                .duration_since(UNIX_EPOCH)
+                .expect("Time went backwards");
+            data.windows.push_back(WindowData {
+                id: since_the_epoch.as_millis() as u64,
+                origin: Point::new(0., 0.),
+                size: Size::new(100., 100.),
+            });
+        }
+    }
+
+    fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &AppState, env: &Env) {
+        self.content.lifecycle(ctx, event, data, env);
+    }
+
+    fn update(&mut self, ctx: &mut UpdateCtx, _old_data: &AppState, data: &AppState, env: &Env) {
+        self.content.update(ctx, data, env);
+    }
+
+    fn layout(
+        &mut self,
+        ctx: &mut LayoutCtx,
+        bc: &BoxConstraints,
+        data: &AppState,
+        env: &Env,
+    ) -> Size {
+        let _content_size = self.content.layout(ctx, bc, data, env);
+        self.content.set_origin(ctx, data, env, Point::new(0., 0.));
+
+        bc.max()
+    }
+
+    fn paint(&mut self, ctx: &mut PaintCtx, data: &AppState, env: &Env) {
+        self.content.paint(ctx, data, env);
+    }
 }
--- src/repeater.rs
+++ src/repeater.rs
@@ -1,12 +1,49 @@
-use druid::{im, Data, Lens, Widget, WidgetPod};
+use druid::widget::{prelude::*, LensWrap};
 use druid::{
-    widget::{prelude::*, LensWrap},
-    Color,
+    im::{self, HashMap, HashSet},
+    Data, Lens, Widget, WidgetPod,
 };
-use std::{marker::PhantomData, vec::Vec};
+use std::{hash::Hash, marker::PhantomData, vec::Vec};
+
+struct RepeaterChild<U, I> {
+    widget:
+        WidgetPod<im::Vector<U>, LensWrap<im::Vector<U>, U, ListItemLens<U>, Box<dyn Widget<U>>>>,
+    id: I,
+}
+
+pub struct ListItemLens<U> {
+    pub index: usize,
+    phantom_u: PhantomData<U>,
+}
+
+impl<U> ListItemLens<U> {
+    pub fn new(index: usize) -> ListItemLens<U> {
+        ListItemLens {
+            index,
+            phantom_u: PhantomData,
+        }
+    }
+}
+
+impl<U> Lens<im::Vector<U>, U> for ListItemLens<U>
+where
+    U: Data,
+{
+    fn with<V, F: FnOnce(&U) -> V>(&self, state: &im::Vector<U>, f: F) -> V {
+        f(&state.get(self.index).unwrap())
+    }
+    fn with_mut<V, F: FnOnce(&mut U) -> V>(&self, state: &mut im::Vector<U>, f: F) -> V {
+        let mut focused = state.get(self.index).unwrap().clone();
+        let result = f(&mut focused);
+        if !focused.same(&state.get(self.index).unwrap()) {
+            state.set(self.index, focused);
+        }
+        result
+    }
+}
 
 pub struct Repeater<T, U, I, L, W> {
-    children: Vec<WidgetPod<im::Vector<U>, Box<dyn Widget<im::Vector<U>>>>>,
+    children: Vec<RepeaterChild<U, I>>,
     list_lens: L,
     id_getter: Box<dyn Fn(&U) -> I>,
     child_generator: Box<dyn Fn(&U) -> W>,
@@ -42,36 +79,129 @@ impl<T, U, I, L, W> Widget<T> for Repeater<T, U, I, L, W>
 where
     T: Data,
     U: Data,
-    I: Data,
+    I: Hash + Eq + Clone + Data,
     L: Lens<T, im::Vector<U>>,
-    W: Widget<U>,
+    W: Widget<U> + 'static,
 {
     fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut T, env: &Env) {
         for child in &mut self.children {
             self.list_lens
-                .with_mut(data, |data| child.event(ctx, event, data, env));
+                .with_mut(data, |data| child.widget.event(ctx, event, data, env));
         }
     }
 
     fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &T, env: &Env) {
         for child in &mut self.children {
             self.list_lens
-                .with(data, |data| child.lifecycle(ctx, event, data, env));
+                .with(data, |data| child.widget.lifecycle(ctx, event, data, env));
         }
     }
 
-    fn update(&mut self, ctx: &mut UpdateCtx, _old_data: &T, data: &T, env: &Env) {
+    fn update(&mut self, ctx: &mut UpdateCtx, old_data: &T, data: &T, env: &Env) {
         for child in &mut self.children {
             self.list_lens
-                .with(data, |data| child.update(ctx, data, env));
+                .with(data, |data| child.widget.update(ctx, data, env));
         }
+
+        let list_lens = &self.list_lens;
+        let id_getter = &self.id_getter;
+        let children = &mut self.children;
+        let child_generator = &self.child_generator;
+
+        list_lens.with(old_data, |old_list| {
+            list_lens.with(data, |list| {
+                // Before we start, check if there are any differences
+                let mut should_diff = false;
+
+                if old_list.len() != list.len() {
+                    should_diff = true;
+                } else {
+                    // If the lists have the same length, loop through and
+                    // check equality on each ID
+                    for i in 0..list.len() {
+                        if !(id_getter)(old_list.get(i).unwrap())
+                            .same(&(id_getter)(list.get(i).unwrap()))
+                        {
+                            should_diff = true;
+                            break;
+                        }
+                    }
+                }
+
+                if !should_diff {
+                    return;
+                }
+
+                // Diffing has three stages: remove, add and sort. For each, it
+                // figures out what of each operation must be performed to
+                // transform old_list into list, and performs those on the
+                // widget list.
+
+                // If we didn't add or remove anything, this will remain false.
+                let mut children_changed = false;
+
+                // Start
+                let mut stale = HashSet::new();
+                for list_item in old_list {
+                    stale.insert((id_getter)(list_item));
+                }
+
+                // Target (key is ID, value is target index)
+                let mut fresh = HashMap::new();
+                for i in 0..list.len() {
+                    fresh.insert((id_getter)(list.get(i).unwrap()), i);
+                }
+
+                // Remove
+                for i in (0..old_list.len()).rev() {
+                    let id = (id_getter)(&old_list.get(i).unwrap());
+                    if !fresh.contains_key(&id) {
+                        children.remove(i);
+                        children_changed = true;
+                    }
+                }
+
+                // Add
+                for item in list {
+                    let id = (id_getter)(item);
+                    if !stale.contains(&id) {
+                        let lens = ListItemLens::<U>::new(0);
+
+                        children.push(RepeaterChild {
+                            widget: WidgetPod::new(LensWrap::new(
+                                Box::new((child_generator)(&item)),
+                                lens,
+                            )),
+                            id,
+                        });
+
+                        children_changed = true;
+                    }
+                }
+
+                // Sort by expected index
+                children.sort_unstable_by(|a, b| fresh.get(&a.id).cmp(&fresh.get(&b.id)));
+
+                // Update the lens indices
+                for i in 0..children.len() {
+                    children[i].widget.widget_mut().lens_mut().index = i;
+                }
+
+                if children_changed {
+                    ctx.children_changed();
+                } else {
+                    ctx.request_layout();
+                }
+            });
+        });
     }
 
     fn layout(&mut self, ctx: &mut LayoutCtx, bc: &BoxConstraints, data: &T, env: &Env) -> Size {
         // TODO
         for child in &mut self.children {
             self.list_lens.with(data, |data| {
-                child.layout(ctx, bc, data, env);
+                let size = child.widget.layout(ctx, bc, data, env);
+                child.widget.set_layout_rect(ctx, data, env, size.to_rect());
             });
         }
 
@@ -81,7 +211,7 @@ where
     fn paint(&mut self, ctx: &mut PaintCtx, data: &T, env: &Env) {
         for child in &mut self.children {
             self.list_lens
-                .with(data, |data| child.paint(ctx, data, env));
+                .with(data, |data| child.widget.paint(ctx, data, env));
         }
     }
 }
