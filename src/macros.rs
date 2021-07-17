7acf29f393b8e6bd17fad1c09bd1a381166873ed
Lisael <lisael@lisael.org>
2021-07-17 05:40:52 +0200

    Accept doc in the selectors and keys macros (#63)
    * accept doc in the selectors and keys macros
    * be more specific for doc attrs in macros

--- src/macros.rs
+++ src/macros.rs
@@ -2,54 +2,76 @@
 /// ```
 /// # use druid_widget_nursery::selectors;
 /// selectors! {
+///     /// Foo the baz
 ///     FOO,
+///     /// Bar the qux yay much
 ///     BAR: usize,
 /// }
 /// ```
 /// expands to
 /// ```
 /// # use druid::Selector;
+/// /// Foo the baz
 /// pub const FOO: Selector = Selector::new("path::to::module::FOO@0:0");
+/// /// Bar the qux yay much
 /// pub const BAR: Selector<usize> = Selector::new("path::to::module::BAR@0:0");
 /// ```
 #[macro_export]
 macro_rules! selectors {
-    ($($name:ident $( : $ty:ty)?),* $(,)?) => {$(
-        pub const $name: ::druid::Selector<$($ty)?> = ::druid::Selector::new(concat!(
-            module_path!(),
-            "::",
-            stringify!($name),
-            "@",
-            line!(),
-            ":",
-            column!()
-        ));
-    )*};
+    (
+        $(
+            $(#[$attr:meta])*
+            $name:ident $( : $ty:ty)?
+        ),* $(,)?
+    ) => {
+        $(
+            $(#[$attr])*
+            pub const $name: ::druid::Selector<$($ty)?> = ::druid::Selector::new(concat!(
+                module_path!(),
+                "::",
+                stringify!($name),
+                "@",
+                line!(),
+                ":",
+                column!()
+            ));
+        )*
+    };
 }
 
 /// Generates `Key`s based on module, line and column
 /// ```
 /// # use druid_widget_nursery::keys;
 /// keys! {
+///     /// height of the bar
 ///     BAR: usize,
 /// }
 /// ```
 /// expands to
 /// ```
 /// # use druid::Key;
+/// /// height of the bar
 /// pub const BAR: Key<usize> = Key::new("path::to::module::BAR@0:0");
 /// ```
 #[macro_export]
 macro_rules! keys {
-    ($($name:ident : $ty:ty),* $(,)?) => {$(
-        pub const $name: ::druid::Key<$ty> = ::druid::Key::new(concat!(
-            module_path!(),
-            "::",
-            stringify!($name),
-            "@",
-            line!(),
-            ":",
-            column!()
-        ));
-    )*};
+    (
+        $(
+            $(#[$attr:meta])*
+            $name:ident : $ty:ty
+        ),* $(,)?
+    ) => {
+        $(
+            $(#[$attr])*
+            pub const $name: ::druid::Key<$ty> = ::druid::Key::new(concat!(
+                module_path!(),
+                "::",
+                stringify!($name),
+                "@",
+                line!(),
+                ":",
+                column!()
+            ));
+        )*
+    };
 }
