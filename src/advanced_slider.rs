fbf29f1841aa8563dfa257c3f7484e74289fc04a
Moritz Eisebitt <moritz.eisebitt@gmail.com>
2021-09-03 11:27:53 +0200

    Added comments and renamed boks to rounded_box

--- src/advanced_slider.rs
+++ src/advanced_slider.rs
@@ -39,18 +39,34 @@ pub struct AdvancedSlider {
 }
 
 impl AdvancedSlider {
+    /// Takes a mouse event and returns the slider value at the specified
+    /// x position.
     fn x_from_mouse(&self, mouse_event: &druid::MouseEvent) -> f64 {
+        // Determines percentage regarding the slider size 120.0
         let mut perc_attempt: f64 = (mouse_event.pos.x - 2.0) / 120.0;
+        // Make sure percentage is bounded between 0 and 1
         if perc_attempt < 0.0 {
             perc_attempt = 0.0;
         } else if perc_attempt > 1.0 {
             perc_attempt = 1.0;
         }
+        // Convert to value
         perc_attempt * (self.max_val - self.min_val) + self.min_val
     }
 
+    /// Takes a data attempt and converts it to valid data, that satisfies the
+    /// stepping, as well as min and max value of the slider. Also returns a
+    /// formated String which is used for the label and satisfies the
+    /// significant digits option.
+    /// 
+    /// When stepping is disabled the returned String is identical to the
+    /// keyboard input and therefore allows more significant digits to be
+    /// displayed to show the exact value.
     fn data_from_attempt(&self, mut data_attempt: f64) -> (f64, String) {
+        // Track whether attempt was out of bounds to correct the input string
+        // in the case of no stepping
         let mut modified = false;
+        // Ensure data is bounded
         if data_attempt < self.min_val {
             data_attempt = self.min_val;
             modified = true;
@@ -62,6 +78,7 @@ impl AdvancedSlider {
         let string: String;
         match self.step_size {
             Some(step_size) => {
+                // Apply stepping
                 data = ((data_attempt - self.min_val) / step_size).round() * step_size + self.min_val;
                 string = String::from(format!("{:.*}", self.signif_dig, data));
             }
@@ -69,11 +86,14 @@ impl AdvancedSlider {
                 data = data_attempt;
                 if self.keyboard_input_origin {
                     if modified {
+                        // Correct String when data was corrected
                         string = String::from(format!("{:.*}", self.signif_dig, data));
                     } else {
+                        // Otherwise take input string from the keyboard
                         string = self.input_string.clone();
                     }
                 } else {
+                    // Format String when the origin wasn't keyboard input
                     string = String::from(format!("{:.*}", self.signif_dig, data));
                 }
             }
@@ -81,6 +101,7 @@ impl AdvancedSlider {
         (data, string)
     }
 
+    /// Builder style method for constructing a new slider
     pub fn new() -> AdvancedSlider {
         AdvancedSlider {
             min_val: 0.0,
@@ -97,6 +118,7 @@ impl AdvancedSlider {
         }
     }
 
+    /// Builder style method for setting the range.
     pub fn with_range(mut self, min_val: f64, max_val: f64) -> AdvancedSlider {
         if min_val < max_val {
             self.min_val = min_val;
@@ -109,12 +131,15 @@ impl AdvancedSlider {
         }
     }
 
+    /// Builder style method for setting the start value.
     pub fn with_start_val(mut self, start_val: f64) -> AdvancedSlider {
         self.start_val = start_val;
         self.val_text = TextLayout::from_text(format!("{:.*}", self.signif_dig, start_val));
         self
     }
 
+    /// Builder style method to set the stepping size. Zero coresponds to no
+    /// (infinite small) stepping.
     pub fn with_step_size(mut self, step_size: f64) -> AdvancedSlider {
         if step_size <= 0.0 {
             self.step_size = None;
@@ -124,11 +149,14 @@ impl AdvancedSlider {
         self
     }
 
+    /// Builder style method to set the significant digits for displaying.
     pub fn with_significant(mut self, signif_dig: usize) -> AdvancedSlider {
         self.signif_dig = signif_dig;
         self
     }
 
+    /// Builder style method to give the label and offset. Sometimes necessary
+    /// on different operating systems with different fonts ? <- Not sure about that
     pub fn with_text_offset(mut self, offset: f64) -> AdvancedSlider {
         self.text_offset = offset;
         self
@@ -146,10 +174,12 @@ impl Widget<f64> for AdvancedSlider {
             }
 
             Event::KeyDown(key_event) => match &key_event.key {
+                // Enter to confirm keyboard input
                 druid::keyboard_types::Key::Enter => {
                     ctx.resign_focus();
                     let try_parse = self.input_string.parse::<f64>();
                     match try_parse {
+                        // When parsable -> specify keyboard origin and convert to data
                         Ok(parsed_input) => {
                             self.keyboard_input_origin = true;
                             let data_tuple = self.data_from_attempt(parsed_input);
@@ -157,6 +187,7 @@ impl Widget<f64> for AdvancedSlider {
                             self.val_text = TextLayout::from_text(format!("{}", data_tuple.1));
                         }
 
+                        // When not parsable -> revert to old data
                         Err(_) => {
                             self.val_text =
                                 TextLayout::from_text(format!("{:.*}", self.signif_dig, data));
@@ -167,6 +198,7 @@ impl Widget<f64> for AdvancedSlider {
                     ctx.request_paint();
                 }
 
+                // Handle allowed input characters
                 druid::keyboard_types::Key::Character(string) => match string.as_str() {
                     "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "." | "-" => {
                         self.input_string.push_str(&string);
@@ -177,6 +209,7 @@ impl Widget<f64> for AdvancedSlider {
                     _ => {}
                 }
 
+                // Handle deleting chararcters of the input sting
                 druid::keyboard_types::Key::Backspace => {
                     self.input_string.pop();
                     self.val_text = TextLayout::from_text(format!("{}", self.input_string));
@@ -188,8 +221,11 @@ impl Widget<f64> for AdvancedSlider {
             }
 
             Event::MouseDown(mouse_event) => {
+                // Make sure the widget only reacts when not in input mode
                 if !self.input_mode {
+                    // Check whether a double click happened
                     if self.last_click.elapsed().as_millis() < 100_u128 {
+                        // Enter input mode
                         self.input_mode = true;
                         self.input_string = String::from("");
                         self.val_text = TextLayout::from_text(format!("{}", self.input_string));
@@ -197,6 +233,7 @@ impl Widget<f64> for AdvancedSlider {
                         ctx.request_focus();
                         ctx.request_paint();
                     } else {
+                        // Handle simple click
                         ctx.set_active(true);
                         let data_attempt = self.x_from_mouse(mouse_event);
                         let data_tuple = self.data_from_attempt(data_attempt);
@@ -207,13 +244,17 @@ impl Widget<f64> for AdvancedSlider {
                 }
             }
 
+            // Handle mouse up and set time instance for double click
             Event::MouseUp(_mouse_event) => {
                 ctx.set_active(false);
                 self.last_click = Instant::now();
             }
 
+            // Handle mouse movement for dragging of the slider
             Event::MouseMove(mouse_event) => {
+                // Make sure the widget only reacts if not in input mode
                 if !self.input_mode {
+                    // Make sure widget only reacts when active
                     if ctx.is_active() {
                         let data_attempt = self.x_from_mouse(mouse_event);
                         let data_tuple = self.data_from_attempt(data_attempt);
@@ -228,6 +269,7 @@ impl Widget<f64> for AdvancedSlider {
         }
     }
 
+    // Handle initialisation
     fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, _data: &f64, _env: &Env) {
         match event {
             LifeCycle::WidgetAdded => {
@@ -265,10 +307,11 @@ impl Widget<f64> for AdvancedSlider {
     }
 
     fn paint(&mut self, ctx: &mut PaintCtx, data: &f64, _env: &Env) {
+        // Handle in which mode to draw the widget
         if self.input_mode {
-            let boks = RoundedRect::new(2.0, 2.0, 122.0, 22.0, 2.0);
-            ctx.fill(boks, &Color::rgb8(50, 50, 50));
-            ctx.stroke(boks, &Color::rgb8(80, 80, 80), 1.0);
+            let rounded_box = RoundedRect::new(2.0, 2.0, 122.0, 22.0, 2.0);
+            ctx.fill(rounded_box, &Color::rgb8(50, 50, 50));
+            ctx.stroke(rounded_box, &Color::rgb8(80, 80, 80), 1.0);
 
             let text_width = self.val_text.layout_metrics().size.width;
             self.val_text.draw(
@@ -276,17 +319,17 @@ impl Widget<f64> for AdvancedSlider {
                 Point::new(62.0 - (text_width / 2.0), 2.0 + self.text_offset),
             );
         } else {
-            let boks = RoundedRect::new(2.0, 2.0, 122.0, 22.0, 2.0);
+            let rounded_box = RoundedRect::new(2.0, 2.0, 122.0, 22.0, 2.0);
             let percentage = (data - self.min_val) / (self.max_val - self.min_val) * 100.0;
             let blocker = Rect::new(percentage * 1.2 + 2.0, 2.0, 122.0, 22.0);
 
             if (data < &self.min_val) | (data > &self.max_val) {
-                ctx.fill(boks, &Color::rgb8(212, 32, 35));
+                ctx.fill(rounded_box, &Color::rgb8(212, 32, 35));
             } else {
-                ctx.fill(boks, &Color::rgb8(41, 128, 186));
+                ctx.fill(rounded_box, &Color::rgb8(41, 128, 186));
                 ctx.fill(blocker, &Color::rgb8(80, 80, 80));
             }
-            ctx.stroke(boks, &Color::rgb8(30, 30, 30), 1.0);
+            ctx.stroke(rounded_box, &Color::rgb8(30, 30, 30), 1.0);
 
             let text_width = self.val_text.layout_metrics().size.width;
             self.val_text.draw(
