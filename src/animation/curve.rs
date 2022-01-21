5edc86d2d95f39ba20a753364790012574d8112f
Dietmar Maurer <dietmar@proxmox.com>
2022-01-21 10:49:31 +0100

    improve AnimationCurve docs
    Signed-off-by: Dietmar Maurer <dietmar@proxmox.com>

--- src/animation/curve.rs
+++ src/animation/curve.rs
@@ -1,3 +1,17 @@
+// Copyright 2019 The Druid Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use std::fmt;
 use std::fmt::{Debug, Formatter};
 use std::f64::consts::PI;
@@ -8,11 +22,20 @@ impl From<fn(f64) -> f64> for AnimationCurve {
     }
 }
 
+/// Animation Curve
+///
 /// An animation curve, mapping from time in the range 0..1 to progress in a range "around" 0..1.
 /// It is permissible for progress to undershoot and overshoot.
+///
+/// Inspired by Robert Penner’s [easing] functions.
+///
+/// [easing]: http://robertpenner.com/easing/
 pub enum AnimationCurve {
+    /// Defined with constant function.
     Function(fn(f64) -> f64),
+    /// Defined with closure.
     Closure(Box<dyn FnMut(f64) -> f64>),
+    /// Defined as Cubic Bezier curve.
     CubicBezier(CubicBezierAnimationCurve),
     //    Spring(SpringAnimationCurve),
 }
@@ -36,10 +59,10 @@ impl Debug for AnimationCurve {
                 .finish(),
             AnimationCurve::CubicBezier(b) => formatter
                 .debug_struct("AnimationCurve::CubicBezier")
-                .field("a", &b.a)
-                .field("b", &b.b)
-                .field("c", &b.c)
-                .field("d", &b.d)
+                .field("x1", &b.x1)
+                .field("y1", &b.y1)
+                .field("x2", &b.x2)
+                .field("y2", &b.y2)
                 .finish(),
         }
     }
@@ -47,36 +70,58 @@ impl Debug for AnimationCurve {
 
 impl AnimationCurve {
 
+    /// F(t) -> t
     pub const LINEAR: Self = Self::Function(|t| t);
 
+    /// F(t) -> t²
     pub const EASE_IN: Self = Self::Function(ease_in);
+    /// Flipped  [`EASE_IN`](AnimationCurve::EASE_IN)
     pub const EASE_OUT: Self = Self::Function(|t| flip_curve(ease_in, t));
+    /// combines [`EASE_IN`](AnimationCurve::EASE_IN) and [`EASE_OUT`](AnimationCurve::EASE_OUT)
     pub const EASE_IN_OUT: Self = Self::Function(|t| combine_in_out(ease_in, t));
 
+    /// Oscillating curve that grows in magnitude while overshooting its bounds.
     pub const EASE_IN_ELASTIC: Self = Self::Function(|t| flip_curve(ease_out_elastic, t));
+    /// Oscillating curve that shrink in magnitude while overshooting its bounds.
     pub const EASE_OUT_ELASTIC: Self = Self::Function(ease_out_elastic);
+    /// Oscillating curve that grows and then shrinks in magnitude
+    /// while overshooting its bounds.
     pub const EASE_IN_OUT_ELASTIC: Self = Self::Function(|t| combine_in_out_rev(ease_out_elastic, t));
 
+    /// F(t) -> 1 - cos(tπ/2)
     pub const EASE_IN_SINE: Self = Self::Function(|t| 1.0 - (t * PI * 0.5).cos());
+    /// F(t) -> sin(tπ/2)
     pub const EASE_OUT_SINE: Self = Self::Function(|t| (t * PI * 0.5).sin());
+    /// combines [`EASE_IN_SINE`](AnimationCurve::EASE_IN_SINE) and [`EASE_OUT_SINE`](AnimationCurve::EASE_OUT_SINE)
     pub const EASE_IN_OUT_SINE: Self = Self::Function(|t| -0.5 * (t * PI).cos() + 0.5);
 
+    /// F(t) -> 2¹⁰⁽ᵗ⁻¹⁾
     pub const EASE_IN_EXPO: Self = Self::Function(ease_in_expo);
+    ///  Flipped  [`EASE_IN_EXPO`](AnimationCurve::EASE_IN_EXPO)
     pub const EASE_OUT_EXPO: Self = Self::Function(|t| flip_curve(ease_in_expo, t));
+    /// combines [`EASE_IN_EXPO`](AnimationCurve::EASE_IN_EXPO) and [`EASE_OUT_EXPO`](AnimationCurve::EASE_OUT_EXPO)
     pub const EASE_IN_OUT_EXPO: Self = Self::Function(|t| combine_in_out(ease_in_expo, t));
 
+    /// A Cubic curve that undershoots slowly a start and ends quickly.
     pub const EASE_IN_BACK: Self = Self::cubic(0.36, 0.0, 0.66, -0.56);
+    /// A Cubic curve that starts quickly and ends with slowly, with an overshoot at the end.
     pub const EASE_OUT_BACK: Self = Self::cubic(0.34, 1.56, 0.64, 1.0);
+    /// combines [`EASE_IN_BACK`](AnimationCurve::EASE_IN_BACK) and [`EASE_OUT_BACK`](AnimationCurve::EASE_OUT_BACK)
     pub const EASE_IN_OUT_BACK: Self = Self::cubic(0.68, -0.6, 0.32, 1.6);
 
+    /// Oscillating curve that grows larger, mimicking a bounce effect
     pub const BOUNCE_IN: Self = Self::Function(|t| flip_curve(bounce, t));
+    /// Flipped [`BOUNCE_IN`](AnimationCurve::BOUNCE_IN)
     pub const BOUNCE_OUT: Self = Self::Function(bounce);
+    /// combines [`BOUNCE_IN`](AnimationCurve::BOUNCE_IN) and [`BOUNCE_OUT`](AnimationCurve::BOUNCE_OUT)
     pub const BOUNCE_IN_OUT: Self = Self::Function(|t|  combine_in_out_rev(bounce, t));
 
-    pub const fn cubic(a: f64, b: f64, c: f64, d: f64) -> Self {
-        Self::CubicBezier(CubicBezierAnimationCurve { a, b, c, d })
+    /// Create a Cubic Bezier curve.
+    pub const fn cubic(x1: f64, y1: f64, x2: f64, y2: f64) -> Self {
+        Self::CubicBezier(CubicBezierAnimationCurve { x1, y1, x2, y2 })
     }
 
+    /// Returns the value of the curve at point `t`.
     pub fn translate(&mut self, t: f64) -> f64 {
         match self {
             Self::Function(f) => f(t),
@@ -85,16 +130,33 @@ impl AnimationCurve {
         }
     }
 
+    /// Create an instance with the given closure.
     pub fn from_closure(f: impl FnMut(f64) -> f64 + 'static) -> AnimationCurve {
         AnimationCurve::Closure(Box::new(f))
     }
 }
 
+/// A [Cubic Bezier] curve where P0 is (0, 0) and P3 is (1, 1)
+///
+/// This is normally used with [AnimationCurve], for eaxple:
+///
+/// ```
+/// use druid_widget_nursery::animation::AnimationCurve;
+///
+/// let curve = AnimationCurve::cubic(0.68, -0.6, 0.32, 1.6);
+///
+/// ```
+///
+/// [Cubic Bezier]: https://en.wikipedia.org/wiki/B%C3%A9zier_curve
 pub struct CubicBezierAnimationCurve {
-    pub a: f64,
-    pub b: f64,
-    pub c: f64,
-    pub d: f64,
+    /// X coordinate of P1
+    pub x1: f64,
+    /// Y coordinate of P1
+    pub y1: f64,
+    /// X coordinate of P2
+    pub x2: f64,
+    /// Y coordinate of P2
+    pub y2: f64,
 }
 
 impl CubicBezierAnimationCurve {
@@ -105,6 +167,7 @@ impl CubicBezierAnimationCurve {
                                           m * m * m
     }
 
+    /// Returns the value of the curve at point `t`.
     pub fn translate(&self, t: f64) -> f64 {
         let mut start = 0.0;
         let mut end = 1.0;
@@ -113,9 +176,9 @@ impl CubicBezierAnimationCurve {
 
         loop {
             let midpoint = (start + end) / 2.0;
-            let estimate = Self::evaluate_cubic(self.a, self.c, midpoint);
+            let estimate = Self::evaluate_cubic(self.x1, self.x2, midpoint);
             if (t - estimate).abs() < CUBIC_ERROR_BOUND {
-                return Self::evaluate_cubic(self.b, self.d, midpoint);
+                return Self::evaluate_cubic(self.y1, self.y2, midpoint);
             }
             if estimate < t {
                 start = midpoint;
